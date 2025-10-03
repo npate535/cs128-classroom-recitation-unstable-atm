d31da1c221e5ce4ff36c0f773b1eb752ad20b94f
Neev Patel <neevpatel@wirelessprv-10-194-156-83.near.illinois.edu>
2025-10-03 10:20:06 -0500

    my message

--- tests/tests.cc
+++ tests/tests.cc
@@ -74,3 +74,35 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
+
+TEST_CASE("Example: PrintLedger", "[ex-4]") {
+Atm atm;
+REQUIRE_THROWS(atm.PrintLedger("register.txt", 3, 4));
+}
+
+TEST_CASE("Example: WithdrawCash", "[ex-5]") {
+Atm atm;
+atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+REQUIRE_THROWS(atm.WithdrawCash(3, 3, 4));
+REQUIRE_THROWS(atm.WithdrawCash(12345678, 1234, -5.00));
+REQUIRE_THROWS(atm.WithdrawCash(12345678, 1234, 500000.00));
+}
+
+TEST_CASE("Example: RegisterAccount", "[ex-6]") {
+Atm atm;
+atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+REQUIRE_THROWS(atm.RegisterAccount(12345678, 1234, "Nev", 500000.00));
+}
+
+TEST_CASE("Example: Deposit", "[ex-7]") {
+Atm atm;
+atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+REQUIRE_THROWS(atm.RegisterAccount(12345678, 1234, "Nev", 500000.00));
+}
+
+TEST_CASE("Example: DepositCash", "[ex-7]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  atm.DepositCash(12345678, 1234, 500.00);
+  REQUIRE(atm.CheckBalance(12345678,1234) == 800.30);
+}
